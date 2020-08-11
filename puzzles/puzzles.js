a1af4a3e04b6f653a5c2afdc08ed1b4e21851f65
Mark-Adaptive <mark.bekker@weareadaptive.com>
2020-08-10 23:30:17 -0400

    Added puzzle file

---
+++ puzzles/puzzles.js
@@ -0,0 +1,38 @@
+export const puzzles = {
+  easy: {
+    level1: {
+      bigNumber: 10,
+      tiles: [4, 5, 3, 2],
+      symbols: {
+        add: 2,
+        subtract: 0,
+        multiply: 0,
+        divide: 1,
+      },
+    },
+  },
+  medium: {
+    level1: {
+      bigNumber: 59,
+      tiles: [6, 2, 3, 7, 3],
+      symbols: {
+        add: 1,
+        subtract: 0,
+        multiply: 2,
+        divide: 1,
+      },
+    },
+  },
+  hard: {
+    level1: {
+      bigNumber: 62,
+      tiles: [1, 2, 8, 4, 3, 2],
+      symbols: {
+        add: 1,
+        subtract: 2,
+        multiply: 2,
+        divide: 0,
+      },
+    },
+  },
+};
