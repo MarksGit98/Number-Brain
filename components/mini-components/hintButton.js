3e9a459f58c1c07af9a49f320c09940e510358d6
MarksGit98 <markbekker1998@gmail.com>
2021-09-06 23:42:40 -0400

    working on hints

--- components/mini-components/hintButton.js
+++ components/mini-components/hintButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TouchableWithoutFeedback, Image, View } from "react-native";
 import { useSelector } from "react-redux";
 import { styles } from "../styles/styles";
@@ -12,6 +12,19 @@ import { volumeSelector, solutionSelector } from "../selectors/stateSelectors";
 export const HintButton = (props) => {
   const volume = useSelector(volumeSelector);
   const solution = useSelector(solutionSelector);
+
+  useEffect(() => {
+    if (solution) {
+      const symbols = { add: 0, subtract: 0, multiply: 0, divide: 0 };
+      solution.forEach((step) => (symbols[step.symbol] += 1));
+      for (let i = 0; i < solution.length; i++) {
+        symbols[solution[i].symbol] -= 1;
+        solution[i] = { ...solution[i], remainingSymbols: { ...symbols } };
+      }
+      console.log(solution);
+    }
+  }, [solution]);
+
   const handleButtonClick = () => {
     if (volume) playSound(BUTTON_CLICK);
     console.log(solution);
